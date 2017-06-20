a58863f0b216f05fe2fedfe10d1ff1b0d1a61a53
em01 <github@em01.co.uk>
2017-06-20 07:59:08 +0700

    Initial Numbers

---
+++ Numbers.playground/Contents.swift
@@ -0,0 +1,12 @@
+//: Playground - noun: a place where people can play
+
+import UIKit
+
+var str = "Hello, playground"
+//Integer signed = positive, zero or negative value - this is the standard
+//Integer unsigned = positive or zero value
+
+var myNumber: Int = -500
+
+//UInt = unsigned integer
+var myNumberTwo: UInt = 500
