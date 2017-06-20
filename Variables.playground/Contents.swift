fb3ec608f1b864e783e4ceb0d25d152fe978d538
em01 <github@em01.co.uk>
2017-06-20 07:45:52 +0700

    Variables playground

--- Variables.playground/Contents.swift
+++ Variables.playground/Contents.swift
@@ -3,3 +3,21 @@
 import UIKit
 
 var str = "Hello, playground"
+
+var myNum = 20
+
+var dec = 50.5
+
+dec = 2.2
+
+//constants-cannot change
+let newDec = 40.4
+
+//multiple vars
+var account1 = 0.1, account2 = 1.1
+
+//If define the data type-no data yet but states when data does come into it then it has to be a string-type annotation
+
+var newMessage: String
+
+var red, blue : Double
