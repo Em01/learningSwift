c9e33ba3670652a32fd2a82fec159af66c7ea9f1
em01 <github@em01.co.uk>
2017-06-21 21:41:30 +0700

    ex two

--- excercise2/Excercise2.playground/Contents.swift
+++ excercise2/Excercise2.playground/Contents.swift
@@ -3,3 +3,21 @@
 import UIKit
 
 var str = "Hello, playground"
+
+var myNumber: Int = 15
+var anotherNumber = 5
+var storedNumbers = myNumber + anotherNumber
+var multNumber = 5 * 9
+var subNumber = myNumber - anotherNumber
+var divNumber = myNumber / anotherNumber
+
+var one = 1, two = 2, three = 3
+
+let constOne = 5, constTwo = 6, constThree = 7
+
+var myDouble: Double
+var myInt: Int = 423
+var myString: String = "Hello"
+var anotherString: String = "World"
+var concatStrings = myString + " " + anotherString
+var again = print("So \(myString) \(anotherString)")
