f8b8bec3ac093b3e2254b10ef6007d968f23c1a9
em01 <github@em01.co.uk>
2017-06-21 20:27:56 +0700

    Numbers and type safety

--- Numbers.playground/Contents.swift
+++ Numbers.playground/Contents.swift
@@ -10,3 +10,15 @@ var myNumber: Int = -500
 
 //UInt = unsigned integer
 var myNumberTwo: UInt = 500
+
+
+var bigNumber: Int64 = 2345678654577777
+
+var test: Double = 2.1
+var myFloat: Float = 2.1
+
+//Float is 6 digits, double is more
+
+//Casting, e.g to work with both the double and float vals you need to convert
+
+var sum = test * Double(myFloat)
