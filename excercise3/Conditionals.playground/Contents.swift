48c385b2929597d48b8890d9f6ddc9cd2ef19fec
em01 <github@em01.co.uk>
2017-06-22 08:17:25 +0700

    Basic conditionals

---
+++ excercise3/Conditionals.playground/Contents.swift
@@ -0,0 +1,35 @@
+//: Playground - noun: a place where people can play
+
+import UIKit
+
+var str = "Hello, playground"
+
+var example: Bool = false
+
+var anotherBool = true
+
+if example {
+    print ("a")
+} else {
+    print("b")
+}
+
+var result = true == true //true
+result = true == false //false 
+result = false == false //true
+
+var accountTotal = 30.33
+var newGame =  59.99
+
+if accountTotal >= newGame {
+    print ("purchased")
+} else {
+    print("Not enough money")
+}
+
+var name = "Anna"
+if name == "Anna" {
+    print("Name is \(name)")
+} else {
+    print("Another name")
+}
