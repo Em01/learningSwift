621864177944f3565fabdb0ef304509efb26181d
em01 <github@em01.co.uk>
2017-07-08 21:26:21 +0700

    More learning

---
+++ notes/Int.playground/Contents.swift
@@ -0,0 +1,11 @@
+//: Playground - noun: a place where people can play
+
+//Interger
+let language = "Swift"
+let year = 2014 // Int
+
+//Floating point - Double or Float, use double unless you know you need a float(6 points)
+
+var version = 3.0 //Double
+
+//Boolean
---
+++ notes/TypeSafety.playground/Contents.swift
@@ -0,0 +1,13 @@
+//: Playground - noun: a place where people can play
+
+import UIKit
+
+var str = "Hello, playground"
+
+var someString = "A"
+//someString = 12.0 would error because Swift is a statically typed language
+
+let bestPlayer: String = "My String"
+let points: Double = 30.1
+let year: Int = 2002
+let boo: Bool = true
