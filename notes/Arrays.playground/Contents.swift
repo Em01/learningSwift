0cfa7cf947bfdfeffeabec36dc166907c5a06517
em01 <github@em01.co.uk>
2017-07-28 14:43:07 +0700

    Arrays

---
+++ notes/Arrays.playground/Contents.swift
@@ -0,0 +1,35 @@
+//Arrays
+
+var todo: [String] = ["A", "B", "C"]
+
+//add single item to end of array
+todo.append("D")
+
+//concat two arrays
+[1, 2, 3] + [4]
+
+todo += ["f"]
+
+
+print(todo)
+
+//immutable arrays
+let secondTaskList = ["Mow the lawn"]
+
+//secondTaskList.append('A') -errors
+todo[0]
+
+//Modifying or Mutating an array
+todo[0] = "Br"
+print(todo)
+
+
+//Insert using indexes
+todo.insert("V", at: 4)
+
+//remove items from arrays
+todo.remove(at: 4)
+
+//array length
+todo.count
+
