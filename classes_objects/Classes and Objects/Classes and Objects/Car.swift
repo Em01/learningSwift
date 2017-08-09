3c13583b914f4099dc3ceaace4a3106ced41a798
em01 <github@em01.co.uk>
2017-08-09 14:22:05 +0700

    Enums

---
+++ classes_objects/Classes and Objects/Classes and Objects/Car.swift
@@ -0,0 +1,22 @@
+//
+//  Car.swift
+//  Classes and Objects
+//
+//  Created by Emma Williams on 09/08/2017.
+//  Copyright © 2017 Emma Williams. All rights reserved.
+//
+
+import Foundation
+
+enum CarType {
+    case Sedan
+    case Coupe
+    case Hatchback
+}
+
+class Car {
+    
+    var color: String = "Black"
+    var numberOfSeats: Int = 5
+    var typeOfCar : CarType = .Coupe
+}
---
+++ classes_objects/Classes and Objects/Classes and Objects/main.swift
@@ -0,0 +1,9 @@
+//
+//  main.swift
+//  Classes and Objects
+//
+//  Created by Emma Williams on 09/08/2017.
+//  Copyright © 2017 Emma Williams. All rights reserved.
+//
+
+import Foundation
