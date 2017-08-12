8b03b7b2f6299cff2f989beb4bebade54932434d
em01 <github@em01.co.uk>
2017-08-12 19:56:08 +0700

    Set up Alamofire

--- clima/Clima/WeatherViewController.swift
+++ clima/Clima/WeatherViewController.swift
@@ -8,6 +8,8 @@
 
 import UIKit
 import CoreLocation
+import Alamofire
+import SwiftyJSON
 
 class WeatherViewController: UIViewController, CLLocationManagerDelegate {
     
@@ -45,7 +47,17 @@ class WeatherViewController: UIViewController, CLLocationManagerDelegate {
     
     //Write the getWeatherData method here:
     
-
+    func getWeatherData(url: String, parameters: [String: String]) {
+        Alamofire.request(url, method: .get, parameters: parameters ).responseJSON {
+            response in
+            if response.result.isSuccess {
+                print("Success! Got the weather data")
+            } else {
+                print("Error \(response.result.error ?? <#default value#>)")
+                self.cityLabel.text = "Connection Issues"
+            }
+        }
+    }
     
     
     
@@ -88,6 +100,8 @@ class WeatherViewController: UIViewController, CLLocationManagerDelegate {
             let longitude = String(location.coordinate.longitude)
             
             let params : [String : String] = ["lat" : latitude, "long" : longitude, "appid" : APP_ID ]
+            
+            getWeatherData(url: WEATHER_URL, parameters: params)
         }
     }
     
